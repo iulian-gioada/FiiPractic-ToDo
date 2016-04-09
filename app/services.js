6835b1e7b748a31420d18d76cfeb33726ffa6581
Bogdan Cochior <bogdan.cochior@thinslices.com>
2016-04-09 14:53:10 +0300

    reformat

--- app/services.js
+++ app/services.js
@@ -1,49 +1,49 @@
 angular.module('todo')
     .factory('todoItemsFactory', itemsFactory)
     .factory('categoriesFactory', categoriesFactory);
-    
-    function itemsFactory() {
-       var items = [];
-       
-       function createItem() {
-           return {
-               title: '',
-               description: '',
-               complete: false,
-               dueDate: '',
-               category: ''
-           }
-       }
-       
-       function getItems() {
-           return items;
-       }
-       
-       function addNewItem (item) {
-           items.push(item);
-       };
-       
-       return {
-           createItem: createItem,
-           getItems: getItems,
-           addNewItem: addNewItem
-       } 
-    };
-    
-    
-    function categoriesFactory(){
-        var initialCategories = ['Home', 'Work', 'Extra'];
-        
+
+function itemsFactory() {
+    var items = [];
+
+    function createItem() {
         return {
-            getCategories: getCategories,
-            addNewCategory: addNewCategory
-        };
-        
-        function getCategories(){
-            return initialCategories;
+            title: '',
+            description: '',
+            complete: false,
+            dueDate: '',
+            category: ''
         }
-        
-        function addNewCategory (item) {
-           initialCategories.push(item);
-       };
     }
+
+    function getItems() {
+        return items;
+    }
+
+    function addNewItem(item) {
+        items.push(item);
+    };
+
+    return {
+        createItem: createItem,
+        getItems: getItems,
+        addNewItem: addNewItem
+    }
+};
+
+
+function categoriesFactory() {
+    var initialCategories = ['Home', 'Work', 'Extra'];
+
+    return {
+        getCategories: getCategories,
+        addNewCategory: addNewCategory
+    };
+
+    function getCategories() {
+        return initialCategories;
+    }
+
+    function addNewCategory(item) {
+        initialCategories.push(item);
+    };
+}
